115ae8f137ac52ab4fbdf5fc4561390f862fefa1
nagisa1993 <mahaiyue2015@163.com>
2017-08-08 19:59:45 -0700

    activity JSON

---
+++ app/src/main/java/com/crowdpp/nagisa/crowdpp2/util/ActivityJson.java
@@ -0,0 +1,31 @@
+package com.crowdpp.nagisa.crowdpp2.util;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by haiyue ma on 7/30/17.
+ */
+
+public class ActivityJson extends JSONObject {
+
+
+    public JSONObject makeJSONObject(String date, String time, ArrayList<String> Activity, String Confidence, String most) {
+
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put("date", date);
+            obj.put("time", time);
+            obj.put("activity", Activity);
+            obj.put("confidence", Confidence);
+            obj.put("mostproac", most);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
+    }
+}
